462a871512c0e2e32faf6265a994f578303d9909
Jumscrafteur <hsansane@gmail.com>
2020-08-04 16:21:07 +0200

    🛠 Setup

---
+++ main.js
@@ -0,0 +1,16 @@
+﻿const express = require('express')
+const routes = require('./routes')
+const app = express()
+const path = require('path')
+const port = process.env.port || 3000;
+
+
+app.set("views", path.resolve(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+﻿var express = require('express');
+
+var router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('index.ejs', {
+    title: 'Site test de jums',
+    message: 'Ceci est le site test de jumscrafteur'
+  })
+})
+
+module.exports = router;
