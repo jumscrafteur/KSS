5ff9d08f93acffd383a49c8feab40df34de1c785
Jumscrafteur <hsansane@gmail.com>
2020-08-04 21:42:52 +0200

    🔗 Ajout Index et Chapitres

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
 ﻿const express = require('express')
+const MongoClient = require('mongodb').MongoClient
 const routes = require('./routes')
 const app = express()
 const path = require('path')
-const port = process.env.port || 3000;
+require('dotenv').config()
+const port = process.env.PORT || 3000;
 
+// const connectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@Cluster0-rmp3c.mongodb.net/test?retryWrites=true&w=majority`
+const connectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dnmds.mongodb.net/<dbname>?retryWrites=true&w=majority`
 
-app.set("views", path.resolve(__dirname, 'views'))
-app.set('view engine', 'ejs')
+MongoClient.connect(connectionString, {
+    useUnifiedTopology: true
+  })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('scans')
 
+    app.set("views", path.resolve(__dirname, 'views'))
+    app.set('view engine', 'ejs')
+    app.use(express.static('public'))
 
-app.use('/', routes);
+    app.use('/', routes);
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`)
-})
+    app.listen(port, () => {
+      console.log(`Listening at http://localhost:${port}`)
+    })
+
+  })
+  .catch(error => {
+    console.error(error)
+  })
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,46 @@
 ﻿var express = require('express');
+const fetch = require('node-fetch');
+const xml2js = require('xml2js');
 
 var router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index.ejs', {
-    title: 'Site test de jums',
-    message: 'Ceci est le site test de jumscrafteur'
+  res.render('index', {
+    title: 'Index'
   })
 })
+
+router.get('/chapitres', async (req, res) => {
+  var test = await fetch('https://mangadex.org/rss/GUp54sazNTkvCX9dYKtwerM28gEfPmBq/group_id/13463')
+  var str = await test.text()
+  var result = await xml2js.parseStringPromise(str)
+  rssData = result.rss.channel[0].item
+  sentData = []
+  for (let i = 0; i < rssData.length; i++) {
+    item = rssData[i]
+    var [, mangaId] = /https:\/\/mangadex\.org\/title\/(\d+)/gm.exec(item.mangaLink[0])
+    var [, titre, chapitre] = /([A-Za-z ]+) - (.+)/gm.exec(item.title[0])
+    var obj = {
+      titre,
+      chapitre,
+      liens: item.link[0],
+      mangaId,
+      couverture: `https://mangadex.org/images/manga/${mangaId}.jpg`
+    }
+    sentData.push(obj)
+
+  }
+  res.render('chapitres', {
+    title: 'Chapitres',
+    sentData
+  })
+  // res.send('👌')
+})
+
+router.get('/discord', (req, res) => {
+  res.redirect('https://discord.gg/XjP3Mxm')
+})
+
+
 
 module.exports = router;
